232f1174377e016ab5f1eca797d43774665742bb
dit <ditazanelli@gmail.com>
2022-02-06 23:47:13 -0500

    page generation

--- index.js
+++ index.js
@@ -1,64 +1,75 @@
 const inquirer = require("inquirer");
-// const generatePage = require("./src/page-template");
-// const { writeFile, copyFile } = require("./utils/generate-site");
+const generatePage = require("./src/template");
+const { writeFile, copyFile } = require("./src/generate-site");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 const promptPM = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: `Enter the Project Manager's name (Required)`,
-      validate: (nameInput) => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log(`Please enter PM's name !`);
-          return false;
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: `name`,
+        message: `Enter the Project Manager's name (Required)`,
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log(`Please enter PM's name !`);
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "email",
-      message: `Enter the Project Manager's email (Required)`,
-      validate: (emailInput) => {
-        if (emailInput) {
-          return true;
-        } else {
-          console.log(`Please enter PM's email !`);
-          return false;
-        }
+      {
+        type: "input",
+        name: "email",
+        message: `Enter the Project Manager's email (Required)`,
+        validate: (emailInput) => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log(`Please enter PM's email !`);
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "id",
-      message: `Enter Project Manager's ID (Required)`,
-      validate: (idInput) => {
-        if (idInput) {
-          return true;
-        } else {
-          console.log(`Please enter PM's ID !`);
-          return false;
-        }
+      {
+        type: "input",
+        name: "id",
+        message: `Enter Project Manager's ID (Required)`,
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log(`Please enter PM's ID !`);
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "office",
-      message: `Enter the office # (Required)`,
-      validate: (officeInput) => {
-        if (officeInput) {
-          return true;
-        } else {
-          console.log(`Please office # !`);
-          return false;
-        }
+      {
+        type: "input",
+        name: "office",
+        message: `Enter the office # (Required)`,
+        validate: (officeInput) => {
+          if (officeInput) {
+            return true;
+          } else {
+            console.log(`Please office # !`);
+            return false;
+          }
+        },
       },
-    },
-  ]);
+    ])
+    .then((managerData) => {
+      this.manager = new Manager(
+        managerData.name,
+        managerData.email,
+        managerData.id,
+        managerData.office
+      );
+    });
 };
-
 
 const promptEmployee = (teamData) => {
   console.log(`
@@ -66,9 +77,9 @@ const promptEmployee = (teamData) => {
    Adding new Employee
   =====================
   `);
-  if (!teamData.employees) {
-    teamData.employees = [];
-  }
+  // if (!teamData.employees) {
+  //   teamData.employees = [];
+  // }
   return inquirer
     .prompt([
       {
@@ -104,16 +115,16 @@ const promptEmployee = (teamData) => {
         choices: [`Engineer`, `Intern`],
       },
       {
-        type: 'input',
-        name: 'github',
+        type: "input",
+        name: "github",
         message: `Employee's github account :`,
-        when: (answers) => answers.employeeRole === 'Engineer'
+        when: (answers) => answers.employeeRole === "Engineer",
       },
       {
-        type: 'input',
-        name: 'school',
+        type: "input",
+        name: "school",
         message: `School where Employee is registered :`,
-        when: (answers) => answers.employeeRole === 'Intern'
+        when: (answers) => answers.employeeRole === "Intern",
       },
       {
         type: "confirm",
@@ -121,37 +132,49 @@ const promptEmployee = (teamData) => {
         message: "Would you like to enter another team member ?",
         default: true,
       },
- ])
-    .then((employeeData) => {
-      teamData.employees.push(employeeData);
-      if (employeeData.confirmNewEmployee) {
+    ])
+    .then((answers) => {
+      if (answers.employeeRole === `Engineer`) {
+        this.employee = new Engineer(
+          answers.name,
+          answers.email,
+          answers.id,
+          answers.github
+        );
+      } else if (answers.employeeRole === `Intern`) {
+        this.employee = new Intern(
+          answers.name,
+          answers.email,
+          answers.id,
+          answers.school
+        );
+      }
+      // })
+      // .then(( answers ) => {
+      // teamData.employees.push(employeeData);
+      if (answers.confirmNewEmployee) {
         return promptEmployee(teamData);
       } else {
         return teamData;
       }
     });
-
-    // function role(aRole) {
-    //   return function (answers) {
-    //     return answers[aRole];
-    //   };
 };
 
 promptPM()
-  .then(promptEmployee);
-  // .then((teamData) => {
-  //   return generatePage(teamData);
-  // })
-  // .then((pageHTML) => {
-  //   return writeFile(pageHTML);
-  // })
-  // .then((writeFileResponse) => {
-  //   console.log(writeFileResponse);
-  //   return copyFile();
-  // })
-  // .then((copyFileResponse) => {
-  //   console.log(copyFileResponse);
-  // })
-  // .catch((err) => {
-  //   console.log(err);
-  // });
+  .then(promptEmployee)
+  .then((teamData) => {
+    return generatePage(teamData);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- src/template.js
+++ src/template.js
@@ -1,18 +1,73 @@
-const generatePage = (name, role, id, email, github) => {
+const generateMembers = teamArr => {
     return `
-      <!DOCTYPE html> 
-      <html lang="en"> 
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
-      </head>
-    
-      <body>
-      </body>
-      </html> 
-      `;
-  };
+    <section class="my-3" id="portfolio">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">Team Members</h2>
+      <div class="flex-row justify-space-between">
+      ${teamArr
+        .filter(({ role }) => `Intern`)
+        .map(({ name, email, id, school}) => {
+          return `
+          <div class="col-12 mb-2 bg-dark text-light p-3">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="email">
+            <a href="mailto:${email}">Email</a>
+            </h5>
+            <h5>${id}</h5>
+            <h5>School : ${school}</h5>
+          </div>
+        `;
+        })
+        .join('')}
 
-  module.exports = generatePage;
+      ${teamArr
+        .filter(({ role }) => !`Engineer`)
+        .map(({ name, email, id, github }) => {
+          return `
+          <div class="col-12 mb-2 bg-dark text-light p-3">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="email">
+            <a href="mailto:${email}">Email</a>
+            </h5>
+            <h5>${id}</h5>
+            <h5>Github : <a href="www.github.com/${github}">${github}</h5>
+          </div>
+        `;
+        })
+        .join('')}
+    
+      </div>
+    </section>
+  `;
+};
+
+module.exports = templateData => {
+  // destructure page data by section
+  const members = templateData;
+
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Manager</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./style.css">
+  </head>
+  
+  <body>
+    <header>
+      <div class="container flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">Team Manager</h1>
+      </div>
+    </header>
+    <main class="container my-5">
+      ${generateMembers(members)}
+    </main>
+  </body>
+  </html>
+  `;
+};
